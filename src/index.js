2fd43934da62f66d9ac8e42b1a66cf0f0694171d
Polo <artem-gribanof@yandex.ru>
2022-10-22 20:47:36 +0300

    иду по теории

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
-import { greeting, element } from "./greeting-jsx";
+import { greeting } from "./greeting-jsx";
 
-function App() {
-	return <div>{greeting}</div>;
+// допустим есть div в html файле
+// <div id="root"></div>
+
+// он корневой, т.к. React DOM будет управлять его содержимым
+// обычно есть только 1 корневой элемент
+
+// Для рендеринга сперва передаётся DOM-элемент в ReactDOM.createRoot();
+// далее передевать React элемент в root.render();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(greeting);
+
+// элементы в React иммутабельны
+// после создания элемента нельзя изменить его потомков или атрибуты
+// пока что есть только 1 способ изменить его - это создать новый элемент и передать его в root.render()
+
+function tick() {
+	const element = (
+		<div>
+			<h1>Hello</h1>
+			<h2>Is {new Date().toLocaleString()}</h2>
+		</div>
+	);
+	root.render(element);
 }
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+// реакт обновляет только то, что необходимо!!
+setInterval(tick, 1e3);
+// несмотря на то, что мы создаём элемент, описывающий всё UI дерево,
+// каждую секунду React DOM изменяет только текстовый узел, содержимое которое изменилось
 
-// root.render(greeting);
-
-createRoot(document.getElementById("root")).render(React.createElement("h1", { className: "greeting" }, "Привет, мир!"));
+// на практике обычно вызывают root.render() 1 раз
+// можно обновлять при помощи компонента состояния
