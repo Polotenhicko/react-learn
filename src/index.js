45487b5e5b4a62fd5b38fdcda489fb0f5c4f3845
Polo <artem-gribanof@yandex.ru>
2022-12-17 15:06:08 +0300

    маленькие правки

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import * as Availab from './contentAvailability';
 import * as Divide from './divideCode';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>
-    <Availab.CustomTextInput />
-    <Availab.Parent />
-    <Availab.OuterClickExample />
-    <Availab.BlurExample />
-  </div>
-);
+// root.render(
+//   <div>
+//     <Availab.CustomTextInput />
+//     <Availab.Parent />
+//     <Availab.OuterClickExample />
+//     <Availab.BlurExample />
+//   </div>
+// );
