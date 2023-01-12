b813757e36072b81b9b3354878b847da233a6e6a
Polo <artem-gribanof@yandex.ru>
2023-01-12 20:59:02 +0300

    теория по перенаправлению рефов

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM, { createRoot } from 'react-dom/client';
 import * as Availab from './contentAvailability';
 import * as Divide from './divideCode';
 import { App as Context } from './context';
+import { FancyButton } from './refs';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div />);
+root.render(
+  <FancyButton>
+    <div>123</div>
+  </FancyButton>
+);
