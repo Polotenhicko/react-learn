b839339fee2554d18a0f4e7d37351c456075ca3f
Polo <artem-gribanof@yandex.ru>
2023-01-20 18:34:54 +0300

    хук состояния

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
-import * as Availab from './contentAvailability';
-import * as Divide from './divideCode';
-import { App as Context } from './context';
-import { FancyButton } from './refs';
-import { Table, Glossary } from './fragments';
+import { Example } from './introHooks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>
-    <Table />
-    <Glossary
-      items={[
-        { id: 0, term: 'Lol', description: 'Mem' },
-        { id: 1, term: 'kek', description: 'Tozhe Mem' },
-      ]}
-    />
-  </div>
-);
+root.render(<Example />);
