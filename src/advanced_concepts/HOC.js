d66dac5daf5d8c31d5075b5e099a35003ce87005
Polo <artem-gribanof@yandex.ru>
2023-03-09 03:03:24 +0300

    теория по хокам

--- src/advanced_concepts/HOC.js
+++ src/advanced_concepts/HOC.js
@@ -59,3 +59,90 @@ class CommentList extends Component {
     );
   }
 }
+
+// и есть ещё компонент с похожей реализацией
+
+class BlogPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogPost: DataSource.getBlogPost(props.id),
+    };
+  }
+
+  componentDidMount() {
+    // подписываемся на уведомление
+    DataSource.addChangeListener(this.handleChange);
+  }
+
+  componentWillUnmount() {
+    // отписываемся
+    DataSource.removeChangeListener(this.handleChange);
+  }
+
+  handleChange = () => {
+    // сохраняем комменты из внешнего источника во внешний
+    this.setState({
+      blogPost: DataSource.getBlogPost(),
+    });
+  };
+
+  render() {
+    return <div>{this.state.blogPost}</div>;
+  }
+}
+
+// разница лишь в вызываемых методах, в остальном они одинаковы
+
+// Оба компонента подписываются на оповещения от DataSource при монтировании.
+// Оба меняют внутреннее состояние при изменении DataSource.
+// Оба отписываются от DataSource при размонтировании.
+
+// мы хотим абстрагировать эту функциональность и использоваь в других компонентах
+
+// создадим функцию withSubscription - будет создавать компоненты и подписывать их на обновленеи DataSource
+// и через пропсы будет передавать новые данные
+
+const CommentListWithSubscription = withSubscription(CommentList, (DataSource) =>
+  DataSource.getComments()
+);
+
+const BlogPostWithSubscription = withSubscription(BlogPost, (DataSource, props) =>
+  DataSource.getBlogPost(props.id)
+);
+
+// первый параметр - оборачиваемый компонент
+// второй параметр - коллбэк, которая извлекает нужные нам данные, получает DataSource и текущие пропсы
+
+function withSubscription(WrappedComponent, selectData) {
+  // возвращает новый компонент
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: selectData(DataSource, props),
+      };
+    }
+
+    componentDidMount() {
+      DataSource.addChangeListener(this.handleChange);
+    }
+
+    componentWillUnmount() {
+      DataSource.removeChangeListener(this.handleChange);
+    }
+
+    handleChange = () => {
+      this.setState({
+        data: selectData(DataSource, this.props),
+      });
+    };
+
+    render() {
+      return <WrappedComponent data={this.state.data} {...this.props} />;
+    }
+  };
+}
+
+// HOC ничего не меняет и не меняет поведение оборачиваемого компонента
+// HOC является чистой функцией без побочных эффектов
