654bb9bb2e948b17d468f2e3fc452832da3f00c4
Polo <artem-gribanof@yandex.ru>
2023-03-22 21:40:06 +0300

    теория

--- src/advanced_concepts/coop_library.js
+++ src/advanced_concepts/coop_library.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import $ from 'jquery';
+import jQuery from 'jquery';
+
+// не скачался chose, вот жопа вместо него
+const $ = function (...args) {
+  const zopa = jQuery(...args);
+  if (!zopa.chosen) {
+    zopa.chosen = function () {
+      console.log('zopa', ...arguments);
+    };
+  }
+  return zopa;
+};
 
 // Взаимодействие со сторонними библиотеками
 
@@ -10,10 +21,81 @@ import $ from 'jquery';
 
 class SomePlugin extends React.Component {
   componentDidMount() {
+    // работает через JQuery с DOM
     this.$el = $(this.el);
   }
 
+  componentWillUnmount() {
+    // Многие JQuery плагины добавляют обработчики DOM
+    // и нам нужно их убрать
+    // Если плагин не может это сделать, то мы должны делать это сами
+    // иначе будет утечка памяти
+  }
+
   render() {
-    <div ref={(el) => (this.el = el)}></div>;
+    // не имеет свойств, не имеет дочерних компонентов
+    // для реакта нет смысла обновлять этот компонент
+    return <div ref={(el) => (this.el = el)}></div>;
   }
 }
+
+// Давайте напишем минимальную обёртку для плагина Chosen, который работает с элементами <select>.
+// Мы хотим предоставить следующий API для нашего компонента-обёртки над <Chosen>:
+
+class Choosen extends React.Component {
+  handleChange = (e) => {
+    this.props.onChange(e.target.value);
+  };
+
+  componentDidMount() {
+    this.$el = $(this.el);
+    this.$el.chosen();
+    // хочу получать уведомления об изменений значений
+    // подпишимся на событие change jQuery
+
+    // не передаём в пропсах обработчик, объявляем свой метод
+    this.$el.on('change', this.handleChange);
+  }
+
+  componentWillUnmount() {
+    this.$el.chosen('destroy');
+    // убираем подписку на событие
+    this.$el.off('change', this.handleChange);
+  }
+
+  // В завершение осталось сделать ещё кое-что. В React пропсы могут изменяться со временем
+  // Документация Chosen предлагает использовать jQuery-метод trigger(), чтобы сообщить об изменениях в оригинальном DOM-элементе.
+  // Мы поручим React заниматься обновлением this.props.children внутри <select>, но нужно добавить метод жизненного цикла
+  // componentDidUpdate(), чтобы уведомлять Chosen про обновление списка дочерних элементов:
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.children !== this.props.children) {
+      this.$el.trigger('chosen:updated');
+    }
+    // Таким способом Chosen узнает, что нужно обновить его DOM-элемент, когда дочерние элементы <select> были обновлены React.
+  }
+
+  render() {
+    // обернули в div, вдруг захотим добавить не один select
+    return (
+      <div>
+        <select className="Choosen-select" ref={(el) => (this.el = el)}>
+          {this.props.children}
+        </select>
+      </div>
+    );
+  }
+}
+
+export function App() {
+  return (
+    <Choosen onChange={(value) => console.log(value)}>
+      <option>Ваниль</option>
+      <option>Шоколад</option>
+      <option>Клубника</option>
+    </Choosen>
+  );
+}
+
+// Интеграция с другими визуальными библиотеками
+// Благодаря гибкости createRoot() React может встраиваться в другие приложения.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
-import { App } from './hooks/hookMemo';
+import { App } from './advanced_concepts/coop_library';
+window.$ = window.jQuery = require('jquery');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
