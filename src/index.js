49ffbc7b9c0a86f390151e41bd0618979a37e9ce
Polo <artem-gribanof@yandex.ru>
2023-04-03 20:40:47 +0300

    react без es6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Navigation as Nav } from './advanced_concepts/profiler';
-import { Profiler } from 'react';
+import { Tick } from './advanced_concepts/es6';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Nav />);
+root.render(<Tick />);
